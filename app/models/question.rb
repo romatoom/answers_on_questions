63094bcff08d9161da2d2fcde9b6bcf69c73bace
Roman Ivanov <romani@epayservices.com>
2022-12-03 15:26:47 +0500

    Implement story "User can add one or more files when creating a question"

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :author, class_name: "User", foreign_key: "author_id"
+  has_many_attached :files
 
   validates :title, :body, presence: true
   validates :title, uniqueness: true
--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -30,6 +30,16 @@ feature 'User can create question', %q(
       end
     end
 
+    scenario 'asks a question with attached files' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'Text text text'
+      attach_file 'File(s)', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Ask'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
     scenario 'asks a question with errors' do
       click_on 'Ask'
 
