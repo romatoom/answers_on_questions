9637d972d4c17c77f8cb7c00f616dd6989ffad5d
Roman Ivanov <romani@epayservices.com>
2023-01-15 11:40:05 +0500

    Delete unwanted comment

--- spec/models/concerns/commenteable_spec.rb
+++ spec/models/concerns/commenteable_spec.rb
@@ -2,6 +2,4 @@ require 'rails_helper'
 
 shared_examples_for 'commenteable' do
   it { should have_many(:comments).dependent(:destroy) }
-
-  # let!(:commenteable) { create(described_class.to_s.underscore.to_sym) }
 end
