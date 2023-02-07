74835acc400b5899a9b13ad6d4821a43a1a19eb8
Roman Ivanov <romani@epayservices.com>
2023-02-07 15:23:24 +0500

    Add feature test for "change_question" subscribe, fix typos

--- spec/features/question/subscriptions_for_question.rb
+++ spec/features/question/subscriptions_for_question.rb
@@ -32,8 +32,8 @@ feature 'User can subscribe to notifications', %q(
 
         scenario 'not send email after create new answer', js: true do
           fill_in 'You can answer the question here', with: 'New answer body'
-
           click_on 'Answer'
+
           sleep 0.1
 
           within '.alerts' do
@@ -69,8 +69,8 @@ feature 'User can subscribe to notifications', %q(
 
         scenario 'send email after create new answer', js: true do
           fill_in 'You can answer the question here', with: 'New answer body'
-
           click_on 'Answer'
+
           sleep 0.1
 
           within '.alerts' do
@@ -90,6 +90,91 @@ feature 'User can subscribe to notifications', %q(
 
           open_email(other_user.email)
           expect(current_email.subject).to eq 'New answer'
+        end
+      end
+    end
+
+    describe 'subscription for update question' do
+      let!(:subscription) { create(:subscription, :change_question, id: 1) }
+
+      context 'when user not have subscribe' do
+        scenario 'can subscribed', js: true do
+          visit question_path(question)
+
+          expect(page).to_not have_content 'Unwatch for update question'
+          click_on 'Watch for update question'
+
+          within '.alerts' do
+            expect(page).to have_content 'You have subscribed to be notified of update question.'
+          end
+        end
+
+        scenario 'not send email after update question', js: true do
+          visit questions_path
+
+          click_on 'Edit'
+          fill_in 'Title', with: 'New question title'
+          click_on 'Save'
+
+          sleep 0.1
+
+          within '.alerts' do
+            expect(page).to have_content 'Question has been edited successfully.'
+          end
+
+          # for NewAnswerJob
+          perform_enqueued_jobs
+
+          # for Mailers
+          perform_enqueued_jobs
+
+          expect(all_emails.count).to be 0
+        end
+      end
+
+      context 'when users have subscribe' do
+        background do
+          create(:users_subscription, user: user, subscription: subscription, question: question)
+          create(:users_subscription, user: other_user, subscription: subscription, question: question)
+        end
+
+        scenario 'can unsubscribed', js: true do
+          visit question_path(question)
+
+          expect(page).to_not have_content 'Watch for update question'
+          click_on 'Unwatch for update question'
+
+          within '.alerts' do
+            expect(page).to have_content 'You have unsubscribed to be notified of update question.'
+          end
+        end
+
+        scenario 'send email after update question', js: true do
+          visit questions_path
+
+          click_on 'Edit'
+          fill_in 'Title', with: 'New question title'
+          click_on 'Save'
+
+          sleep 0.1
+
+          within '.alerts' do
+            expect(page).to have_content 'Question has been edited successfully.'
+          end
+
+          # for NewAnswerJob
+          perform_enqueued_jobs
+
+          # for Mailers
+          perform_enqueued_jobs
+
+          expect(all_emails.count).to be 2
+
+          open_email(user.email)
+          expect(current_email.subject).to eq 'Update question'
+
+          open_email(other_user.email)
+          expect(current_email.subject).to eq 'Update question'
         end
       end
     end
