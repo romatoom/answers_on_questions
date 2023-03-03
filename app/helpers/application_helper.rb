4096116bcc71f4b7db2dd36649e9db27cd6ffcde
Roman Ivanov <romani@epayservices.com>
2023-03-03 20:43:55 +0500

    Add fragment cache for question

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,11 @@ module ApplicationHelper
       content_tag :div, message, class: "alert alert-#{ending_alert_class_name}" if flash[key]
     end.join.html_safe
   end
+
+  def collection_cache_key_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+  end
 end
